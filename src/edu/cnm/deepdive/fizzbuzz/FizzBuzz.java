81ab1cbcc2446b87673926984fcff7212219f434
Sky Link <s.z_link@yahoo.com>
2017-06-13 16:19:50 -0600

    Exception Handling

--- src/edu/cnm/deepdive/fizzbuzz/FizzBuzz.java
+++ src/edu/cnm/deepdive/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,12 @@
 package edu.cnm.deepdive.fizzbuzz;
 
 public class FizzBuzz {
-  
-  //default upper limit
+
+  // default upper limit
   public static final int DEF_LIM = 100;
+  public static final String USAGE = "Usage:%n"
+      + "\tjava [options] edu.cnm.deepdive.fizzbuzz.FizzBuzz [limit]%n" + "%n" + "where%n"
+      + "\toptions = VM options%n" + "\tlimit = positive upper counting limit (default = 100)%n";
 
   private static void fizzBuzzCounter(int lim) {
     for (int i = 1; i <= lim; i++) {
@@ -19,10 +22,22 @@ public class FizzBuzz {
     }
   }
 
+  private static void emitUsage() {
+    System.out.printf(USAGE);
+  }
+
   public static void main(String[] args) {
     if (args.length > 0) {
-      Integer limit = new Integer(args[0]);
-      fizzBuzzCounter(limit);
+      try {
+        Integer limit = new Integer(args[0]);
+        if (limit < 0) {
+          throw new IllegalArgumentException("Upper limit must be positive");
+        }
+        fizzBuzzCounter(limit);
+      } catch (IllegalArgumentException ex) {
+        ex.printStackTrace();
+        emitUsage();
+      }
     } else {
       fizzBuzzCounter(DEF_LIM);
     }
